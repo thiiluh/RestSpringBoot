02d34dc914deb8cc722a5d9186305d3bee7e03bd
Thiago <thiluiz.oliveira@gmail.com>
2020-12-09 16:14:15 -0300

    initial struct

---
+++ src/test/java/com/thiago/restwithspringudemy/RestWithSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.restwithspringudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
